226c48373a87edc85c3e5c9a0aeeb874fdc21f9c
ChristopherHX <christopher.homberger@web.de>
2021-12-08 12:46:48 +0100

    Misc

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
@@ -6,7 +7,9 @@
 #include <playapi/device_info.h>
 #include <playapi/login.h>
 #include <playapi/file_login_cache.h>
-#include <string> 
+#include <playapi/checkin.h>
+#include <playapi/util/config.h>
+#include <string>
 #include <jnivm/vm.h>
 #include <jnivm/env.h>
 
@@ -16,33 +19,6 @@
 
 void (*mcpelauncher_preinithook)(const char*sym, void*val, void **orig);
 
-signed char scrollval = 0;
-
-char (*Mouse_feed_org)(char, char, short, short, short, short);
-void Mouse_feed(char a, char b, short c, short d, short e, short f) {
-    if(a == 4 && b != 0) {
-        scrollval = (signed char&)b;
-        if(scrollval < 0) {
-            Mouse_feed_org(a, b, c, d, e, f);
-        }
-    } else {
-        Mouse_feed_org(a, b, c, d, e, f);
-    }
-}
-void (*enqueueButtonPressAndRelease)(void*q, unsigned int, int, int);
-void enqueueButtonPressAndRelease_hook(void*q, unsigned int c, int d, int e) {
-    enqueueButtonPressAndRelease(q, c, d, e);
-    printf("enqueueButtonPressAndRelease_hook: %d %d %d\n", c, d, e);
-    abort();
-}
-void (*MouseMapper_tick_org)(void*a,void*b,void*c);
-void MouseMapper_tick(void*a,void*b,void*c) {
-    MouseMapper_tick_org(a, b, c);
-    if (scrollval > 0 && enqueueButtonPressAndRelease) {
-        enqueueButtonPressAndRelease(b, 425082297, 0, 0);
-        scrollval = 0;
-    }
-}
 int (*JNI_OnLoad_) (void*,void*);
 void* pthread_getattr_np_org;
 
@@ -50,8 +26,6 @@ void* mremap_fake(void *old_address, size_t old_size,
                     size_t new_size, int flags, ...) {
         return MAP_FAILED;
 }
-void*_ZNK11AppPlatform12isLANAllowedEv;
-void*__ZNK11AppPlatform12isLANAllowedEv;
 
 extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
     auto h = dlopen("libmcpelauncher_mod.so", 0);
@@ -63,155 +37,113 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
     try {
         playapi::device_info device;
         auto cachePath = getenv("GPLAY_TOKEN_CACHE_PATH");
+        std::string cachePathStorage;
+        auto suffix = "playapi_token_cache.conf";
         if(!cachePath) {
-            return;
+            /*Dl_info info;
+            if(!dladdr(&mod_preinit, &info)) {
+                return;
+            }
+            cachePathStorage = info.dli_fname ? info.dli_fname : "";
+            auto fslash = cachePathStorage.rfind('/');
+            if(fslash == std::string::npos) {
+                return;
+            }
+            cachePathStorage*/
+            std::vector<std::string> paths;
+            auto cacheBasePath = getenv("XDG_CACHE_HOME");
+            if(cacheBasePath) {
+                paths.push_back(cacheBasePath);
+            }
+            auto home = getenv("HOME");
+            if(home) {
+                std::string shome = home;
+                paths.push_back(shome + "/.cache");
+                paths.push_back(shome + "/Library/Caches");
+                paths.push_back(shome + "/.var/app/io.mrarm.mcpelauncher/cache");
+            }
+            for(auto&&path : paths) {
+                auto finalPath = path + "/Minecraft Linux Launcher/Minecraft Linux Launcher UI/" + suffix;
+                if(!access(finalPath.data(), R_OK)) {
+                    cachePathStorage = finalPath;
+                    cachePath = cachePathStorage.data();
+                    break;
+                }
+            }
+            auto localAppdata = getenv("LOCALAPPDATA");
+            if(localAppdata) {
+                auto finalPath = localAppdata + "/Minecraft Linux Launcher/Minecraft Linux Launcher UI/cache/" + suffix;
+                if(!access(finalPath.data(), R_OK)) {
+                    cachePathStorage = finalPath;
+                    cachePath = cachePathStorage.data();
+                }
+            }
+            if(!cachePath) {
+                return;
+            }
+        } else {
+            cachePathStorage = cachePath;
+        }
+        auto callback_meta = []() {
+            mcpelauncher_preinithook("pthread_getattr_np", (void*)+[](pthread_t th, pthread_attr_t* attr) -> int {
+                return 1;
+            }, &pthread_getattr_np_org);
+            mcpelauncher_preinithook("pthread_attr_getstack", (void*)+[](pthread_attr_t* attr, void **stackaddr, size_t *stacksize) -> int {
+                return 1;
+            }, nullptr);
+            mcpelauncher_preinithook("mremap", (void*)&mremap_fake, nullptr);
+        };
+        auto sres = cachePathStorage.rfind(suffix);
+        if(sres != std::string::npos) {
+            playapi::config c;
+            std::ifstream fs((cachePathStorage.substr(0, sres) + "/misc.cache").data());
+            if(fs.is_open()) {
+                c.load(fs);
+                std::ifstream f(cachePathStorage.data());
+                if(f.is_open()) {
+                    std::stringstream s;
+                    s << f.rdbuf();
+                    auto buf = s.str();
+                    if(c.get("path") == cachePathStorage && c.get_long("idx") == (cachePathStorage.length() + buf.length())) {
+                        callback_meta();
+                        return;
+                    }
+                }
+            }
         }
         playapi::file_login_cache cache(cachePath);
         playapi::login_api login_api(device, cache);
-        // login_api.set_token(getenv("GPLAY_MAIL"), getenv("GPLAY_TOKEN"));
         login_api.set_token("l@localhost", "a");
         playapi::api api(device);
         playapi::checkin_result checkinResult;
-        checkinResult.time = 0;
-        auto sid = getenv("GPLAY_CHECKIN_ANDROID_ID");
-        if(!sid) {
-            return;
-        }
-        checkinResult.android_id = std::stoull(sid);
-        checkinResult.security_token = 0;
-        auto devver = getenv("GPLAY_CHECKIN_DEVICE_DATA_VERSION_INFO");
-        if(!devver) {
-            return;
-        }
-        checkinResult.device_data_version_info = devver;
+        playapi::checkin_api checkin(loginHelper->getDevice());
+        checkin.add_auth(loginHelper->getLoginApi())->call();
+        checkinResult = checkin.perform_checkin()->call();
         api.set_checkin_data(checkinResult);
         api.set_auth(login_api)->call();
-        api.details("com.mojang.minecraftpe")->call([&api](playapi::proto::finsky::response::ResponseWrapper&& resp) {
+        api.details("com.mojang.minecraftpe")->call([&](playapi::proto::finsky::response::ResponseWrapper&& resp) {
             auto details = resp.payload().detailsresponse().docv2();
             if(details.details().appdetails().versionstring() == "" || !details.details().appdetails().versioncode()) {
                 abort();
             }
-            api.delivery("com.mojang.minecraftpe", details.details().appdetails().versioncode(), std::string())->call([](playapi::proto::finsky::response::ResponseWrapper&& resp) {
+            api.delivery("com.mojang.minecraftpe", details.details().appdetails().versioncode(), std::string())->call([&](playapi::proto::finsky::response::ResponseWrapper&& resp) {
                 auto dd = resp.payload().deliveryresponse().appdeliverydata();
                 auto url = (dd.has_gzippeddownloadurl() ? dd.gzippeddownloadurl() : dd.downloadurl());
                 printf("Apk Url: %s\n", url.data());
                 if(url == "") {
                     printf("Invalid Url\n");
                 } else {
-#if defined(__arm__) || defined(__aarch64__)
-                    // Enable by default after mouse bug is resolved
-                    if(getenv("MCPELAUNCHER_ENABLE_SCROLL_HOOK") != nullptr) {
-                        mcpelauncher_preinithook("_ZN5Mouse4feedEccssss", (void*)&Mouse_feed, (void**)&Mouse_feed_org);
-                        mcpelauncher_preinithook("_ZN11MouseMapper4tickER15InputEventQueueR23ControllerIDtoClientMap", (void*)&MouseMapper_tick, (void**)&MouseMapper_tick_org);
+                    if(sres != std::string::npos) {
+                        try {
+                            std::ofstream fs((cachePathStorage.substr(0, sres) + "/misc.cache").data());
+                            playapi::config c;
+                            c.set("path", cachePathStorage);
+                            c.set_long("idx", cachePathStorage.length() + buf.length());
+                            c.save(fs);
+                        } catch(...) {
+                        }
                     }
-#endif
-
-                    mcpelauncher_preinithook("pthread_getattr_np", (void*)+[](pthread_t th, pthread_attr_t* attr) -> int {
-                        return 1;
-                    }, &pthread_getattr_np_org);
-                    mcpelauncher_preinithook("pthread_attr_getstack", (void*)+[](pthread_attr_t* attr, void **stackaddr, size_t *stacksize) -> int {
-                        return 1;
-                    }, nullptr);
-                    mcpelauncher_preinithook("mremap", (void*)&mremap_fake, nullptr);
-                    //mcpelauncher_preinithook("_ZNK11AppPlatform17supportsScriptingEv", (void*)+[](void* t) -> bool { abort() ;return true; }, nullptr);
-                    __ZNK11AppPlatform12isLANAllowedEv = (void*)+[](void*** t) -> bool {
-                        auto mc = dlopen("libminecraftpe.so", 0);
-
-                        auto appPlat = (void**)dlsym(mc, "_ZTV11AppPlatform");
-                        auto raw = &appPlat[2];
-                        auto othervt = *t;
-#ifndef NDEBUG
-                        printf("AppPlatform:\n");
-                        for(int i = 0; raw[i] && raw[i] != (void*)0xffffffffffffffe8; i++) {
-                            Dl_info data;
-                            printf("%p (%s)\n", raw[i], dladdr(raw[i], &data) ? data.dli_sname : "(unknown)");
-                        }
-                        printf("othervt:\n");
-                        for(int i = 0; othervt[i] && othervt[i] != (void*)0xffffffffffffffe8; i++) {
-                            Dl_info data;
-                            printf("%p (%s)\n", othervt[i], dladdr(othervt[i], &data) ? data.dli_sname : "(unknown)");    
-                        }
-#endif
-                        auto _ZNK11AppPlatform19supportsFilePickingEv = (void**)dlsym(mc, "_ZNK11AppPlatform19supportsFilePickingEv");
-                        auto _ZNK11AppPlatform17supportsScriptingEv = (void**)dlsym(mc, "_ZNK11AppPlatform17supportsScriptingEv");
-                        auto _ZNK11AppPlatform25getPlatformUIScalingRulesEv = (void**)dlsym(mc, "_ZNK11AppPlatform25getPlatformUIScalingRulesEv");
-                        auto _ZNK11AppPlatform18supportsWorldShareEv = (void**)dlsym(mc, "_ZNK11AppPlatform18supportsWorldShareEv");
-                        auto _ZNK11AppPlatform20getLevelSaveIntervalEv = (void**)dlsym(mc, "_ZNK11AppPlatform20getLevelSaveIntervalEv");
-                        auto _ZNK11AppPlatform10getEditionEv = (void**)dlsym(mc, "_ZNK11AppPlatform10getEditionEv");
-                        auto _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv = (void**)dlsym(mc, "_ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv");
-                        auto _ZNK11AppPlatform23supports3rdPartyServersEv = (void**)dlsym(mc, "_ZNK11AppPlatform23supports3rdPartyServersEv");
-                        auto _ZNK11AppPlatform29allowsResourcePackDevelopmentEv = (void**)dlsym(mc, "_ZNK11AppPlatform29allowsResourcePackDevelopmentEv");
-                        auto _ZNK11AppPlatform30supportsAutoSaveOnDBCompactionEv = (void**)dlsym(mc, "_ZNK11AppPlatform30supportsAutoSaveOnDBCompactionEv");
-                        auto _ZNK11AppPlatform23isAutoCompactionEnabledEv = (void**)dlsym(mc, "_ZNK11AppPlatform23isAutoCompactionEnabledEv");
-                        auto _ZN11AppPlatform22uiOpenRenderDistScalarEv =  (void**)dlsym(mc, "_ZN11AppPlatform22uiOpenRenderDistScalarEv");
-
-                        for(int i = 0; raw[i] && raw[i] != (void*)0xffffffffffffffe8; i++) {
-                            if(raw[i] == _ZNK11AppPlatform19supportsFilePickingEv) {
-                                othervt[i] = (void*) +[](void*t) -> bool {
-                                    printf("_ZNK11AppPlatform19supportsFilePickingEv called\n");
-                                    return true;
-                                };
-                                printf("Patched _ZNK11AppPlatform19supportsFilePickingEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform17supportsScriptingEv) {
-                                othervt[i] = (void*) +[](void*t) -> bool {
-                                    printf("_ZNK11AppPlatform17supportsScriptingEv called\n");
-                                    return true;
-                                };
-                                printf("Patched _ZNK11AppPlatform17supportsScriptingEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform25getPlatformUIScalingRulesEv) {
-                                othervt[i] = (void*) +[](void*t) -> int {
-                                    printf("_ZNK11AppPlatform25getPlatformUIScalingRulesEv called\n");
-                                    return 0;
-                                };
-                                printf("Patched _ZNK11AppPlatform25getPlatformUIScalingRulesEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform18supportsWorldShareEv) {
-                                othervt[i] = (void*) +[](void*t) -> bool {
-                                    printf("_ZNK11AppPlatform18supportsWorldShareEv called\n");
-                                    return true;
-                                };
-                                printf("Patched _ZNK11AppPlatform18supportsWorldShareEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform10getEditionEv) {
-                                othervt[i] = (void*) +[](void*t) -> std::string {
-                                    printf("_ZNK11AppPlatform10getEditionEv called\n");
-                                    return "win10";
-                                };
-                                printf("Patched _ZNK11AppPlatform10getEditionEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv) {
-                                othervt[i] = (void*) +[](void*t) -> int {
-                                    printf("_ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv called\n");
-                                    return 100;
-                                };
-                                printf("Patched _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv\n");
-                            }
-                            if(raw[i] == _ZNK11AppPlatform29allowsResourcePackDevelopmentEv) {
-                                othervt[i] = (void*) +[](void*t) -> bool {
-                                    printf("_ZNK11AppPlatform29allowsResourcePackDevelopmentEv called\n");
-                                    return false;
-                                };
-                                printf("Patched _ZNK11AppPlatform29allowsResourcePackDevelopmentEv\n");
-                            }
-                            if(raw[i] == _ZN11AppPlatform22uiOpenRenderDistScalarEv) {
-                                othervt[i] = (void*) +[](void*t) -> int {
-                                    printf("_ZN11AppPlatform22uiOpenRenderDistScalarEv called\n");
-                                    return 512;
-                                };
-                                printf("Patched _ZN11AppPlatform22uiOpenRenderDistScalarEv\n");
-                            }
-                            if(othervt[i] == __ZNK11AppPlatform12isLANAllowedEv) {
-                                othervt[i] = _ZNK11AppPlatform12isLANAllowedEv;
-                                printf("Patched __ZNK11AppPlatform12isLANAllowedEv back to org\n");
-                            }
-                        }
-
-                        dlclose(mc);    
-                        return true;
-                    };
-                    mcpelauncher_preinithook("_ZNK11AppPlatform12isLANAllowedEv", __ZNK11AppPlatform12isLANAllowedEv, &_ZNK11AppPlatform12isLANAllowedEv);
+                    callback_meta();
                 }
             }, [](std::exception_ptr e) {
             });
@@ -219,10 +151,4 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
         });
     } catch(...) {
     }
-}
-
-extern "C" __attribute__ ((visibility ("default"))) void mod_init() {
-    auto mc = dlopen("libminecraftpe.so", 0);
-    enqueueButtonPressAndRelease = (decltype(enqueueButtonPressAndRelease))dlsym(mc, "_ZN15InputEventQueue28enqueueButtonPressAndReleaseEj11FocusImpacti");
-    dlclose(mc);
 }
