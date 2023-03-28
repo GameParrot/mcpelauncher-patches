2c0154ff6beaa50f5c43fdccddc0ea3f83dd053c
ChristopherHX <christopher.homberger@web.de>
2023-03-28 16:08:21 +0200

    call funcs

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
 
+int nestedchild() {
+    printf("enter nestedchild\n");
+    printf("exit nestedchild\n");
+    return 23;
+}
+
+
+int child() {
+    printf("enter child\n");
+    int r = nestedchild();
+    printf("nestedchild: %d\n", r);
+    printf("exit child\n");
+    return 54;
+}
+
 void __attribute__((constructor)) mod_preinit() {
     printf("Hello World\n");
+    int r = child();
+    printf("child: %d\n", r);
 }
