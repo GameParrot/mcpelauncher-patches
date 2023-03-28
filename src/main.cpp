c2cda7bd00bc907908d71ac032439bb0ab58885f
ChristopherHX <christopher.homberger@web.de>
2023-03-28 15:02:37 +0200

    replace std::cout with printf

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+//#include <iostream>
+#include <cstdlib>
 
 extern "C" void __attribute__((constructor)) mod_preinit() {
-    std::cout << "Hello World\n";
-    std::cout << "From android\n";
+    // std::cout << "Hello World\n";
+    // std::cout << "From android\n";
+    fprintf("Hello World\n");
 }
