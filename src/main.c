049b14831f3d8dfa3a986b0361f43facf71e92e2
ChristopherHX <christopher.homberger@web.de>
2023-03-29 20:13:53 +0200

    .

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int child() {
     printf("enter child\n");
     int r = nestedchild();
     printf("nestedchild:");
-    putc('0' + r);
+    putchar('0' + r);
     printf("\n");
     printf("exit child\n");
     return 3;
@@ -21,6 +21,6 @@ void __attribute__((constructor)) mod_preinit() {
     printf("Hello World\n");
     int r = child();
     printf("child:");
-    putc('0' + r);
+    putchar('0' + r);
     printf("\n");
 }
