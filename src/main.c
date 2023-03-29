2bd7a95fb05f1be602bf257c581cd959d833f143
ChristopherHX <christopher.homberger@web.de>
2023-03-29 20:08:55 +0200

    don't use variadic

--- src/main.c
+++ src/main.c
@@ -3,20 +3,30 @@
 int nestedchild() {
     printf("enter nestedchild\n");
     printf("exit nestedchild\n");
-    return 23;
+    return 6;
 }
 
 
 int child() {
     printf("enter child\n");
     int r = nestedchild();
-    printf("nestedchild: %d\n", r);
+    printf("nestedchild:");
+    char buf[2];
+    buf[0] = '0' + r;
+    buf[1] = (char)0;
+    printf(buf);
+    printf("\n");
     printf("exit child\n");
-    return 54;
+    return 3;
 }
 
 void __attribute__((constructor)) mod_preinit() {
     printf("Hello World\n");
     int r = child();
-    printf("child: %d\n", r);
+    printf("child:");
+    char buf[2];
+    buf[0] = '0' + r;
+    buf[1] = (char)0;
+    printf(buf);
+    printf("\n");
 }
