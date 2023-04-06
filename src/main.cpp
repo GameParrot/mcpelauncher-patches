baa0cd88643ed434de3aa5bd3f20627c568b2f9f
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-04-06 14:03:05 -0400

    Remove printf

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,18 @@ void*_ZNK11AppPlatform12isLANAllowedEv;
 void*__ZNK11AppPlatform12isLANAllowedEv;
 
 extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
-    printf("Loading mod\n");
     auto h = dlopen("libmcpelauncher_mod.so", 0);
     if(!h) {
         return;
     }
     mcpelauncher_preinithook = (decltype(mcpelauncher_preinithook)) dlsym(h, "mcpelauncher_preinithook");
     
-    printf("Loading mod %p\n", mcpelauncher_preinithook);
     __ZNK11AppPlatform12isLANAllowedEv = (void*)+[](void*** t) -> bool {
-        printf("__ZNK11AppPlatform12isLANAllowedEv\n");
         auto mc = dlopen("libminecraftpe.so", 0);
         
         auto appPlat = (void**)dlsym(mc, "_ZTV11AppPlatform");
         auto raw = &appPlat[2];
         auto othervt = *t;
-        printf("AppPlatform:\n");
-        for(int i = 0; raw[i] && raw[i] != (void*)0xffffffffffffffe8; i++) {
-            Dl_info data;
-            printf("%p (%s)\n", raw[i], dladdr(raw[i], &data) ? data.dli_sname : "(unknown)");
-        }
-        printf("othervt:\n");
-        for(int i = 0; othervt[i] && othervt[i] != (void*)0xffffffffffffffe8; i++) {
-            Dl_info data;
-            printf("%p (%s)\n", othervt[i], dladdr(othervt[i], &data) ? data.dli_sname : "(unknown)");
-        }
         auto _ZNK11AppPlatform19supportsFilePickingEv = (void**)dlsym(mc, "_ZNK11AppPlatform19supportsFilePickingEv");
         auto _ZNK11AppPlatform17supportsScriptingEv = (void**)dlsym(mc, "_ZNK11AppPlatform17supportsScriptingEv");
         auto _ZNK11AppPlatform25getPlatformUIScalingRulesEv = (void**)dlsym(mc, "_ZNK11AppPlatform25getPlatformUIScalingRulesEv");
@@ -45,49 +32,36 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
         for(int i = 0; raw[i] && raw[i] != (void*)0xffffffffffffffe8; i++) {
             if(raw[i] == _ZNK11AppPlatform19supportsFilePickingEv) {
                 othervt[i] = (void*) +[](void*t) -> bool {
-                    printf("_ZNK11AppPlatform19supportsFilePickingEv called\n");
                     return true;
                 };
-                printf("Patched _ZNK11AppPlatform19supportsFilePickingEv\n");
             }
             if(raw[i] == _ZNK11AppPlatform17supportsScriptingEv) {
                 othervt[i] = (void*) +[](void*t) -> bool {
-                    printf("_ZNK11AppPlatform17supportsScriptingEv called\n");
                     return true;
                 };
-                printf("Patched _ZNK11AppPlatform17supportsScriptingEv\n");
             }
             if(raw[i] == _ZNK11AppPlatform25getPlatformUIScalingRulesEv) {
                 othervt[i] = (void*) +[](void*t) -> int {
-                    printf("_ZNK11AppPlatform25getPlatformUIScalingRulesEv called\n");
                     return 0;
                 };
-                printf("Patched _ZNK11AppPlatform25getPlatformUIScalingRulesEv\n");
             }
             if(raw[i] == _ZNK11AppPlatform23getMaxSimRadiusInChunksEv) {
                 othervt[i] = (void*) +[](void*t) -> int {
-                    printf("_ZNK11AppPlatform23getMaxSimRadiusInChunksEv called\n");
                     return 12;
                 };
-                printf("Patched _ZNK11AppPlatform23getMaxSimRadiusInChunksEv\n");
             }
             if(raw[i] == _ZNK11AppPlatform10getEditionEv) {
                 othervt[i] = (void*) +[](void*t) -> std::string {
-                    printf("_ZNK11AppPlatform10getEditionEv called\n");
                     return "win10";
                 };
-                printf("Patched _ZNK11AppPlatform10getEditionEv\n");
             }
             if(raw[i] == _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv) {
                 othervt[i] = (void*) +[](void*t) -> int {
-                    printf("_ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv called\n");
                     return 8;
                 };
-                printf("Patched _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv\n");
             }
             if(othervt[i] == __ZNK11AppPlatform12isLANAllowedEv) {
                 othervt[i] = _ZNK11AppPlatform12isLANAllowedEv;
-                printf("Patched __ZNK11AppPlatform12isLANAllowedEv back to org\n");
             }
         }
         
