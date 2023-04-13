f4656d4bd39a2cb6dc47ec451a5f542881a4a19e
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-04-13 18:03:12 -0400

    _ZNK11AppPlatform16getBuildPlatformEv with hive patch test
    Needs more testing
    Use alt if possible until more testing has been done

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,39 @@
 #include <dlfcn.h>
 #include <sys/mman.h>
 #include <string>
+#include <thread>
+#include <unistd.h>
+#include <netdb.h>
 
 void (*mcpelauncher_preinithook)(const char*sym, void*val, void **orig);
 
 void*_ZNK11AppPlatform12isLANAllowedEv;
 void*__ZNK11AppPlatform12isLANAllowedEv;
+int bp = 8;
+int patchId = 0;
 
 extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
     auto h = dlopen("libmcpelauncher_mod.so", 0);
 
     mcpelauncher_preinithook = (decltype(mcpelauncher_preinithook)) dlsym(h, "mcpelauncher_preinithook");
+
+    mcpelauncher_preinithook("getaddrinfo", (void*)+[](const char *node, const char *service, const addrinfo *hints, addrinfo **res) -> int {
+        if (node) {
+            if (strcmp(node, "geo.hivebedrock.network") == 0) {
+                patchId++;
+                std::thread([=]() {
+                    int patchIdOld = patchId;
+                    bp = 1;
+                    sleep(3);
+                    if (patchIdOld == patchId) {
+                        bp = 8;
+                    }
+                }).detach();
+            }
+        }
+        return getaddrinfo(node, service, hints, res);
+    }, nullptr);
+
     
     __ZNK11AppPlatform12isLANAllowedEv = (void*)+[](void*** t) -> bool {
         auto mc = dlopen("libminecraftpe.so", 0);
@@ -26,6 +49,7 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
         auto _ZNK11AppPlatform23getMaxSimRadiusInChunksEv = (void**)dlsym(mc, "_ZNK11AppPlatform23getMaxSimRadiusInChunksEv");
         auto _ZNK11AppPlatform10getEditionEv = (void**)dlsym(mc, "_ZNK11AppPlatform10getEditionEv");
         auto _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv = (void**)dlsym(mc, "_ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv");
+        auto _ZNK11AppPlatform16getBuildPlatformEv = (void**)dlsym(mc, "_ZNK11AppPlatform16getBuildPlatformEv");
 
         for(int i = 0; raw[i] && raw[i] != (void*)0xffffffffffffffe8; i++) {
             if(raw[i] == _ZNK11AppPlatform19supportsFilePickingEv) {
@@ -56,6 +80,11 @@ extern "C" void __attribute__ ((visibility ("default"))) mod_preinit() {
             if(raw[i] == _ZNK11AppPlatform27getDefaultNetworkMaxPlayersEv) {
                 othervt[i] = (void*) +[](void*t) -> int {
                     return 8;
+                };
+            }
+            if(raw[i] == _ZNK11AppPlatform16getBuildPlatformEv) {
+                othervt[i] = (void*) +[](void*t) -> int {
+                    return bp;
                 };
             }
             if(othervt[i] == __ZNK11AppPlatform12isLANAllowedEv) {
